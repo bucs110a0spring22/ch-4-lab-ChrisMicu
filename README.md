5284967cb66b4c504330d6b5f66400ddd29bbe55
Christopher Micu <98533751+ChrisMicu@users.noreply.github.com>
2022-03-07 17:09:15 +0000

    Completed Lab

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+I get a turtle to draw first a weird sin curve. Then the turtle will create an axis and start to plot the sin cos and tan curves.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ Geeks for Geeks
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import math
 import turtle
+
+dart = turtle.Turtle()
+
+def drawSineCurve(dart):
+  
+  for angle in range(-360 , 361):
+    y = math.sin(math.radians(angle))
+    dart.goto(angle, y )
+    dart.pendown()
+  dart.pu()
+
 
 ########### Your Code here ##############
 # You should only have functions here
@@ -6,13 +18,43 @@ import turtle
 # then you do not fully understand functions
 # and should review how they work or ask for help
 
+def setupWindow(wn):
+  wn.bgcolor('lightblue')
+  wn.setworldcoordinates(-360,-1,360,1)
+  
+def setupAxis(dart):
+
+  dart.pu()
+  dart.goto(-360,0)
+  dart.pd()    
+  dart.goto(360,0)
+  dart.pu()
+  dart.goto(0,-1)
+  dart.pd()
+  dart.goto(0,1)
+  dart.pu()
+  dart.goto(0 , 0)
+  
+
+def drawCosineCurve(dart):
+  
+  for angle in range(-360 , 361):
+    y = math.cos(math.radians(angle))
+    dart.goto(angle, y )
+    dart.pd()
+  dart.pu()
 
 
+def drawTangentCurve(dart):
+  
+  for angle in range(-360 , 361):
+    y = math.tan(math.radians(angle))
+    dart.goto(angle, y )
+    dart.pd()
+  dart.pu()
 
 
-
-
-
+  
 ##########  Do Not Alter Any Code Past Here ########
 def main():
     #Part A
@@ -25,7 +67,7 @@ def main():
     #Part B
     setupWindow(wn)
     setupAxis(dart)
-    dart.speed(0)
+    dart.speed(1)
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
