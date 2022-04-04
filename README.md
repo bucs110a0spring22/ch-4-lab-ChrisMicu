d98b109f367ef2ec6f9dc7e4cdaeececcaabbc30
Christopher Micu <98533751+ChrisMicu@users.noreply.github.com>
2022-04-04 23:11:54 +0000

    Midterm

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 #### CS 110
-# Chapter 4 - Lab - Functions
-
-### [Assignment Description](https://docs.google.com/document/d/1V20D_upUX4MO8YmskKlRB25Yu2pCEv3-h8z4EAfrSno/edit?usp=sharing)
+# Midterm - Python Programming
 
 ***
 
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
-I get a turtle to draw first a weird sin curve. Then the turtle will create an axis and start to plot the sin cos and tan curves.
+What did you clean up?:
+My code seemed clean and nothing needed to be previously cleaned up
 
-## GRACE DAYS
-Grace days used for this assignment: 0
+Summary of function(s) added:
+The first function I added drew a flower, and my larger and interactive functions worked together in order to work like an online calculator which takes a degree and translates it to a radian
 
-Grace days remaining: 5/5
+Summary of Feature Added:
+I wanted to incorporate code that can interact with the user. The first part of the code is me giving the user a rose so that they have a good time using my code. Then the degree to radian "calculator" comes up and the user can select what degree they want to convert to as well as for whichever trigonometric identity they would like. Lastly, if the user would like to see what these curves look like and get a visual representation of the trig identities they can. Once again I allow the user to enter the colors of everything that is being drawn in order to allow them to see everything well.
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
  None
 
 ## REFERENCES:
- Geeks for Geeks
+ Geeks for geeks
 
 ## MISCELLANEOUS COMMENTS:
 None
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import math
 import turtle
+from math import pi
 
 dart = turtle.Turtle()
 
+ 
 def drawSineCurve(dart):
-  
+  sineColor= str(input("Enter color for sin "))
   for angle in range(-360 , 361):
     y = math.sin(math.radians(angle))
     dart.goto(angle, y )
     dart.pendown()
+    dart.pencolor(sineColor)
   dart.pu()
 
 
@@ -17,13 +20,14 @@ def drawSineCurve(dart):
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
-
+  
 def setupWindow(wn):
   wn.bgcolor('lightblue')
   wn.setworldcoordinates(-360,-1,360,1)
   
 def setupAxis(dart):
-
+  graphColor= str(input("Enter color for graph "))
+  dart.pencolor(graphColor)
   dart.pu()
   dart.goto(-360,0)
   dart.pd()    
@@ -37,19 +41,21 @@ def setupAxis(dart):
   
 
 def drawCosineCurve(dart):
-  
+  cosColor= str(input("Enter color for cos "))
   for angle in range(-360 , 361):
     y = math.cos(math.radians(angle))
     dart.goto(angle, y )
+    dart.pencolor(cosColor)
     dart.pd()
   dart.pu()
 
 
 def drawTangentCurve(dart):
-  
+  tanColor= str(input("Enter color for tan "))
   for angle in range(-360 , 361):
     y = math.tan(math.radians(angle))
     dart.goto(angle, y )
+    dart.pencolor(tanColor)
     dart.pd()
   dart.pu()
 
@@ -57,12 +63,12 @@ def drawTangentCurve(dart):
   
 ##########  Do Not Alter Any Code Past Here ########
 def main():
+    
     #Part A
     wn = turtle.Screen()
     wn.tracer(5)
     dart = turtle.Turtle()
     dart.speed(0)
-    drawSineCurve(dart)
 
     #Part B
     setupWindow(wn)
@@ -72,4 +78,98 @@ def main():
     drawCosineCurve(dart)
     drawTangentCurve(dart)
     wn.exitonclick()
+
+    
+#Midterm
+  
+##Code
+def drawFlower():
+  dart=turtle.Turtle()
+  dart.speed(0)
+  dart.goto(0,0)
+print("You are beautiful enjoy this rose")
+wn = turtle.Screen()
+wn.bgcolor("pink")
+wn.setworldcoordinates(-150,-200,250,200)
+dart.shape("turtle")
+dart.color("firebrick","red")
+
+
+for i in range(36):
+  dart.forward(200)
+  dart.left(170)
+  dart.stamp()
+wn.clear()
+
+###Convert Degree to Radian Code##
+
+def factor(n):
+  fact = 1
+  for i in range(1,n+1):
+    fact = fact * i
+  return fact
+  
+def convert(value):
+  return value * pi / 180
+
+def sin(y,n):
+  sine = 0
+  for i in range(n):
+    sn = (-1)**i
+    sine = sine + ((y**(2.0*i+1))/factor(2*i+1))*sn
+  return sine
+
+
+def cos(y,n):
+  cose = 0
+  for i in range(n):
+    sn = (-1)**i
+    cose = cose + ((y**(2.0*i))/factor(2*i))*sn
+  return cose
+
+
+def printMenu():
+
+  while(True):
+
+    print("SELECT A TRIG IDENTITY")
+    print("1 - Calculate the sine of a value")
+    print("2 - Calculate the cosine of a value")
+    print("3 - Calculate the tangent of a value")
+    print("4 - See what the curves look like")
+
+
+    userInput = int(input("Enter your option: "))
+
+    if userInput == 1:
+      value = int(input("Enter the value (in degrees): "))
+      radians = convert(value)
+      result = sin(radians, 10)
+      print("The sine of",value,"is %.4f" %result)
+    elif userInput == 2:
+      value = int(input("Enter the value (in degrees): "))
+      radians = convert(value)
+      result = cos(radians, 10)
+      print("The cos of",value,"is %.4f" %result)
+  
+    elif userInput == 3:
+      value = int(input("Enter the value (in degrees): "))
+      
+      if value == 90 or value == 270:
+        print("The tangent of",value,"is undefined")
+      else:
+        radians = convert(value)
+        result = sin(radians,10) * cos(radians,10) * sin(radians,10) * cos(radians,10)
+        print("The tangent of",value,"is %.4f" %result)
+  
+    elif userInput == 4:
+      break
+    else:
+      print("Invalid Option. Please try again")
+printMenu()
+
+
+'''
+    Want to add more of the user controlling these charts. So what color the sin, cos, and tan should be drawn can be chosen by the user. As well as allowing the user to alter the graph and see what it looks like when being drawn at a different radian. Additionally, something to change the entire chart would be to add intersection points and show where they intersect.
+  '''
 main()
